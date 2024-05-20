77f34f6325d1226243a7e9085cbbf7f22f9a5d60
HELLINFIX <111268251+HELLINFIX@users.noreply.github.com>
2024-05-20 18:10:48 +0000

    spaced: Drop Schedtune

--- device.mk
+++ device.mk
@@ -386,7 +386,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.mt6781 \
     fstab.mt6781_ramdisk \
-    init.cgroup.rc \
+    init.mt6781.power.rc \
     init.connectivity.rc \
     init.modem.rc \
     init.mt6781.rc \
